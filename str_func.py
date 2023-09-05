d0d90b3ebbf0eb92bb830ab16962c7544cb0ec6e
Anastasiya Brodskaya <anastasia.brodskaja@yandex.ru>
2023-09-05 22:09:19 +0300

    have written docstring in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def capitalize_all_letters(string):
+    """функция возвращает строку
+       с заглавной буквы"""
+
     return string.upper()
+
+
+def capitalize_first_letters(string):
+    """которая делает заглавными
+    первые буквы каждого слова"""
+
+    return string.title()
